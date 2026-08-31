de1271728cf47b826ca297945de38935d52314ec
ALEJANDRO <alejo.oliveros80@gmail.com>
2026-08-31 06:54:19 -0500

    "Se agrega tabla contactos al script SQL"

--- TallerPráctico_Csharp/ScriptsSQL/script_bd.sql
+++ TallerPráctico_Csharp/ScriptsSQL/script_bd.sql
@@ -4,5 +4,14 @@ GO
 USE [taller_practico];
 GO
 
+create table contactos
+(
+documento int primary key,
+nombre varchar (100),
+telefono varchar (100),
+correo varchar (100),
+ciudad varchar (100),
+);
+Go
 
 -- Cambio realizado en la rama de desarrollo --
